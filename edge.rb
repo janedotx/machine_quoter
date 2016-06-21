36d2b34cd2be584757eda02fa0d1e715366d67e9
Jane Huang <jane.huang@opower.com>
2016-06-20 22:45:48 -0700

    Changed arc length calculation method.

--- edge.rb
+++ edge.rb
@@ -1,8 +1,15 @@
 class Edge
+  PI = 3.14159
   attr_reader :center, :vertices, :clockwise_vertex_id, :length, :radius
   attr_accessor :next
   attr_accessor :concave, :convex
   attr_reader :positive_x, :negative_x, :positive_y, :negative_y
+
+  class << self
+    def get_dot_product(v1, v2)
+      v1.x * v2.x + v1.y * v2.y
+    end
+  end
 
   def initialize(edge_hash)
     # All id keys will be strings
@@ -22,6 +29,25 @@ class Edge
     @radius = get_distance(vertices_hash[v_id], center)
   end
 
+  def translate_edge(vertices_hash)
+    v1 = vertices_hash[vertices[0]]
+    v2 = vertices_hash[vertices[1]]
+    translated_v1 = Vertex.new(v1.x - center.x, v1.y - center.y)
+    translated_v2 = Vertex.new(v2.x - center.x, v2.y - center.y)
+    [translated_v1, translated_v2]
+  end
+
+  # Return angle in radians.
+  def get_angle(vertices_hash)
+    t_v1, t_v2 = translate_edge(vertices_hash)
+    dot_product = self.class.get_dot_product(t_v1, t_v2)
+    origin = Vertex.new(0,0)
+    magnitude_t_v1 = get_distance(origin, t_v1)
+    magnitude_t_v2 = get_distance(origin, t_v2)
+    cosine = dot_product / (magnitude_t_v1 * magnitude_t_v2)
+    Math.acos(cosine)
+  end
+
   def find_farthest_points(vertices_hash)
     @positive_x = center.x + radius
     @positive_y = center.y + radius
@@ -34,14 +60,16 @@ class Edge
     Math.sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2)
   end
 
-  def find_arc_length
-    raise 'no radius yet' unless @radius
-    3.14159 * @radius
+  def find_arc_length(vertices_hash)
+    set_radius(vertices_hash) unless @radius
+    circumference = 2 *PI * @radius
+    angle = get_angle(vertices_hash)
+    circumference * (angle / (2 * PI))
   end
 
   def find_length(vertices_hash)
     @length ||= if circular
-        find_arc_length
+        find_arc_length(vertices_hash)
       else
         get_distance(vertices_hash[vertices[0]], vertices_hash[vertices[1]])
       end
--- spec/edge_spec.rb
+++ spec/edge_spec.rb
@@ -1,11 +1,14 @@
 require 'awesome_print'
 
-require './plethora'
+require './edge'
+require './vertex'
 
 describe Edge do
   let(:v1) { Vertex.new(0.0, 3.0) }
   let(:v2) { Vertex.new(3.0, 3.0) }
   let(:v3) { Vertex.new(3.0, 0.0) }
+  let(:v4) { Vertex.new(2.0, 3.0) }
+  let(:v5) { Vertex.new(4.0, 1.0) }
 
   let(:vertices_hash) { { '1' => v1, '2' => v2, '3' => v3 } }
 
@@ -22,6 +25,26 @@ describe Edge do
         'ClockwiseFrom' => '3'
       }
     )
+  end
+
+  let(:right_angle_arc) do
+    described_class.new(
+      {
+        'Type' => 'CircularArc',
+        'Vertices' => [1, 3],
+        'Center' => {
+          'X' => 0.0,
+          'Y' => 0.0
+        },
+        'ClockwiseFrom' => '7'
+      }
+    )
+  end
+
+  describe '#get_dot_product' do
+    it 'should calculate the dot product!' do
+      Edge.get_dot_product(v4, v5).should eq 11.0
+    end
   end
 
   describe '.initialize' do
@@ -54,10 +77,26 @@ describe Edge do
   describe '.find_length' do
     it 'should return the correct length' do
       straight_edge.find_length(vertices_hash).should eq 3.0
-      arc.set_radius(vertices_hash)
       arc.find_length(vertices_hash).round(2).should eq 4.71
+      right_angle_arc.find_length(vertices_hash).round(2).should eq 4.71
     end
   end
 
+  describe '.translate_edge' do
+    it 'should give the coordinates of the edge relative to the center' do
+      v1, v2 = arc.translate_edge(vertices_hash)
+      v1.x.should eq 0
+      v1.y.should eq 1.5
+      v2.x.should eq 0
+      v2.y.should eq -1.5
+    end
+  end
+
+  describe '.get_angle' do
+    it 'should get the angle described by an arc' do
+      arc.get_angle(vertices_hash).round(5).should eq Edge::PI
+      right_angle_arc.get_angle(vertices_hash).round(5).should eq (Edge::PI/2).round(5)
+    end
+  end
 end
 
